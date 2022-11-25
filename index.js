05007acab155590bc754b4c2b18c7713e56602c7
tutulportal <tutulvpn3@gmail.com>
2022-11-25 21:25:03 +0600

    server basic setup with user save and get complete

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewire
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2u6wmpl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
+async function run() {
+    try{
+        const usersCollection = client.db('smartPhonez').collection('usersCollection');
+
+
+        // jwt check
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token, haveUser: 1 });
+            }
+            res.status(403).send({ accessToken: '', haveUser: 0 })
+        });
+
+
+        // find all users
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+
+        // insert new user on register
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
+app.get('/', (req, res) => {
+    res.send('Smart Phonez Node Server is Running...');
+});
+
+app.listen(port, () => {
+    console.log(`Smart Phonez Node Server is Running On ${port}`);
+})
